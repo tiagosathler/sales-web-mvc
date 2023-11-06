6d3da78329c59d5c1b2f927187f47d001e74a6ea
Tiago Sathler <sathler@gmail.com>
2023-11-06 13:35:35 -0300

    :sparkles: feat: include js core (tooltip and scroll)

--- SalesWebMvc/wwwroot/js/site.js
+++ SalesWebMvc/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.onload = (e) => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+    document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+        el.addEventListener('shown.bs.tab', () => {
+            const target = el.getAttribute('data-bs-target')
+            const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+            bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+        })
+    });
+}
